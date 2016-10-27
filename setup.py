a3535fc932789bb54dcbdef58b1a1157be53ded9
detonavomek <detonavomek@gmail.com>
2016-10-27 13:08:39 +0300

    v1.1: update unit tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "privoxy_pool",
-    version = "1.0",
+    version = "1.1",
     author = "detonavomek",
     author_email = "detonavomek@gmail.com",
     description = ("Pool Privoxies controller"),
--- test_controller.py
+++ test_controller.py
@@ -1,9 +1,16 @@
-import time
+import time, urllib2, json
+
+import requests
 
 from privoxy_pool import PrivoxyController
 
 
 privoxy_controller = PrivoxyController(maximum_instances=3)
+
+def get_ip(port):
+	proxies = {"http" : "http://127.0.0.1:"+str(port)}
+	content = requests.get('http://api.ipify.org?format=json', proxies=proxies).content
+	return json.loads(content)['ip']
 
 print 'RUN'
 privoxy_controller.run()
@@ -11,7 +18,7 @@ privoxy_controller.run()
 print 'START'
 for i in range(10):
 	port = privoxy_controller.next()
-	print port
+	print "http://127.0.0.1:{port} --> {ip}".format(port=port, ip=get_ip(port))
 	privoxy_controller.set_bad(port)
 
 print 'STOP'
